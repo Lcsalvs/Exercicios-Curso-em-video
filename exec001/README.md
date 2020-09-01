3c864fd99351c00276aa4e3e7005a03cbc85a1bc
Lucas Alves da Conceição <lcsalves1999@gmail.com>
2020-09-01 02:40:02 -0300

    Finalizando o primeiro exercício
    Concluído o primeiro exercício de JS do curso de JS do canal Curso em Vídeo.

--- exec001/README.md
+++ exec001/README.md
@@ -1,2 +1,4 @@
-# Exerc01-Curso-em-video
+# Exerc-Curso-em-video
  Exercícios feito no curso de JS do canal 'Curso em Vídeo'
+
+ O primeiro exercício é uma página que mostra uma imagem e uma cor de fundo diferente, de acordo com a hora informada. 
--- exec001/script.js
+++ exec001/script.js
@@ -1,1 +1,24 @@
-alert("Olá!")
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours() //Pode-se alterar a variável para a hora desejada e verificar cada condição abaixo.
+
+    msg.innerHTML = `Agora são ${hora} horas`
+
+    //Condicional que verifica a hora e retorna uma imagem e uma cor de fundo associada.
+
+    if(hora >= 0 && hora < 12){
+        //BOM DIA
+        img.src = 'manha.png'
+        document.body.style.background = '#e2cd9f'
+    }else if(hora >= 12 && hora <= 18){
+        //BOA TARDE
+        img.src ='tarde.png'
+        document.body.style.background = 'orange'
+    }else{
+        //BOA NOITE
+        img.src = 'noite.png'
+        document.body.style.background = '#515154'
+    }
+}
